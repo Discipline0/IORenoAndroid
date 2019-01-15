b19a4a176fcd92c44e9ea8a650871384e1bda400
Greco <grecoftw@gmail.com>
2019-01-15 13:48:03 -0500

    Create project now inserts values in db, only gallery photo picker missing

--- app/src/main/java/com/ioreno/grecoantoine/ioreno/CustomerCreateNewProject.java
+++ app/src/main/java/com/ioreno/grecoantoine/ioreno/CustomerCreateNewProject.java
@@ -1,6 +1,8 @@
 package com.ioreno.grecoantoine.ioreno;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.media.Image;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
@@ -14,13 +16,21 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.ioreno.grecoantoine.ioreno.Fragments.about_frag;
 import com.ioreno.grecoantoine.ioreno.Fragments.contact_frag;
 import com.ioreno.grecoantoine.ioreno.Fragments.index_frag;
 import com.ioreno.grecoantoine.ioreno.Fragments.terms_of_use_frag;
+
+import java.io.ByteArrayOutputStream;
+
+import DBManager.DBSQLiteManager;
+import Model.Customer;
+import Model.Project;
 
 public class CustomerCreateNewProject extends AppCompatActivity {
 
@@ -48,8 +58,55 @@ public class CustomerCreateNewProject extends AppCompatActivity {
         Spinner  projTypeIn = (Spinner)findViewById(R.id.spinnerCreateNewProjectType);
         EditText projAddressIn = (EditText) findViewById(R.id.editCreateNewProjectAddress);
         EditText projCityIn = (EditText) findViewById(R.id.editCreateNewProjectCity);
-        EditText projBudgetyIn = (EditText) findViewById(R.id.numCreateNewProjectBudget);
-       // Image    projImageIn = (Image) findViewById(R.id.imgProjectImage);
+        EditText projBudgetIn = (EditText) findViewById(R.id.numCreateNewProjectBudget);
+        ImageView projImageIn = (ImageView) findViewById(R.id.imgProjectImage);
+
+        TextView projErrorIn   = (TextView)findViewById(R.id.txtCreateNewProjectError);
+        projErrorIn.setSingleLine(false);
+
+        String projTitle = projTitleIn.getText().toString().trim();
+        String projDescripion = projDescriptionIn.getText().toString().trim();
+        String projType = projTypeIn.getSelectedItem().toString();
+        String projAddress = projAddressIn.getText().toString().trim();
+        String projCity = projCityIn.getText().toString().trim();
+        double projBudget = 0;
+
+        //imageview to bitmap to byte[]
+        Bitmap bitmap = ((BitmapDrawable) projImageIn.getDrawable()).getBitmap();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] imageInByte = baos.toByteArray();
+        //save your stuff);
+
+        DBSQLiteManager db = new DBSQLiteManager(this);
+
+        String error = "";
+
+        try{
+            projBudget = Integer.parseInt(projBudgetIn.getText().toString().trim());
+        }
+        catch(Exception e){
+            error+= "Invalid Budget";
+        }
+
+        if(error.equals("")){
+            Project p = new Project(Customer.currUser, projDescripion, projType, projBudget, projTitle, projAddress, projCity, imageInByte);
+            db.addProject(p);
+            Toast toast = Toast.makeText(getApplicationContext(),
+                    "Project: "+p.getTitle()+" has sucessfully been created!",
+                    Toast.LENGTH_SHORT);
+            Intent i = new Intent(this, CustomerHome.class);
+            startActivity(i);
+
+            toast.show();
+
+        }
+        else{
+            projErrorIn.setText(error);
+        }
+
+
+
 
 
     }
