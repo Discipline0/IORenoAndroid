34d92448bb5b5481b3de7b72262c120f1d7439b4
Greco <grecoftw@gmail.com>
2019-02-14 14:08:59 -0500

    admin finished, 3 home pages too

--- app/src/main/java/com/ioreno/grecoantoine/ioreno/AdminDeniedPayments.java
+++ app/src/main/java/com/ioreno/grecoantoine/ioreno/AdminDeniedPayments.java
@@ -1,6 +1,8 @@
 package com.ioreno.grecoantoine.ioreno;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
@@ -8,7 +10,17 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.ioreno.grecoantoine.ioreno.DBManager.DBSQLiteManager;
+import com.ioreno.grecoantoine.ioreno.Model.Payment;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.ArrayList;
 
 public class AdminDeniedPayments extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
     //  private DrawerLayout dl;
@@ -30,6 +42,125 @@ public class AdminDeniedPayments extends AppCompatActivity implements Navigation
 
         NavigationView nv = findViewById(R.id.nav_view);
         nv.setNavigationItemSelectedListener(this);
+
+        //table
+        TableLayout tl = (TableLayout)findViewById(R.id.DeniedTable);
+
+        //header
+        TableRow tr_head = new TableRow(this);
+        tr_head.setBackgroundColor(getResources().getColor(R.color.TableBlue));
+        tr_head.setLayoutParams(new TableLayout.LayoutParams(
+                TableLayout.LayoutParams.FILL_PARENT,
+                TableLayout.LayoutParams.WRAP_CONTENT));
+
+        //Header***************************************************
+        TextView payment_id = new TextView(this);
+        payment_id.setText("Payment ID");
+        payment_id.setTextColor(Color.BLACK);
+        payment_id.setPadding(5, 5, 30, 5);
+        payment_id.setTypeface(null, Typeface.BOLD);
+        tr_head.addView(payment_id);// add the column to the table row here
+
+        TextView company_id = new TextView(this);
+        company_id.setText("Company #");
+        company_id.setTextColor(Color.BLACK);
+        company_id.setPadding(5, 5, 30, 5);
+        company_id.setTypeface(null, Typeface.BOLD);
+        tr_head.addView(company_id);// add the column to the table row here
+
+        TextView payment_amt = new TextView(this);
+        payment_amt.setText("Payment Amount");
+        payment_amt.setTextColor(Color.BLACK);
+        payment_amt.setPadding(5, 5, 30, 5);
+        payment_amt.setTypeface(null, Typeface.BOLD);
+        tr_head.addView(payment_amt);// add the column to the table row here
+
+        TextView proposal_id = new TextView(this);
+        proposal_id.setText("Proposal ID");
+        proposal_id.setTextColor(Color.BLACK);
+        proposal_id.setPadding(5, 5, 30, 5);
+        proposal_id.setTypeface(null, Typeface.BOLD);
+        tr_head.addView(proposal_id);// add the column to the table row here
+
+        TextView payment_status = new TextView(this);
+        payment_status.setText("Payment Status");
+        payment_status.setTextColor(Color.BLACK);
+        payment_status.setPadding(5, 5, 30, 5);
+        payment_status.setTypeface(null, Typeface.BOLD);
+        tr_head.addView(payment_status);// add the column to the table row here
+
+        TextView payment_date = new TextView(this);
+        payment_date.setText("Payment Status");
+        payment_date.setTextColor(Color.BLACK);
+        payment_date.setPadding(5, 5, 30, 5);
+        payment_date.setTypeface(null, Typeface.BOLD);
+        tr_head.addView(payment_date);// add the column to the table row here
+
+        tl.addView(tr_head, new TableLayout.LayoutParams(
+                TableLayout.LayoutParams.FILL_PARENT,
+                TableLayout.LayoutParams.WRAP_CONTENT));
+
+        int count = 0;
+        ArrayList<Payment> pay_list = getPaymentList();
+        for(Payment p : pay_list){
+            if(p.getPaymentStatus() == 0) {
+                TableRow tr = new TableRow(this);
+                if (count % 2 != 0)
+                    tr.setBackgroundColor(getResources().getColor(R.color.TableBlue));
+                tr.setLayoutParams(new TableLayout.LayoutParams(
+                        TableRow.LayoutParams.FILL_PARENT,
+                        TableRow.LayoutParams.WRAP_CONTENT));
+                TextView paymentID = new TextView(this);
+                TextView companyNum = new TextView(this);
+                TextView paymentAMT = new TextView(this);
+                TextView proposalID = new TextView(this);
+                TextView paymentStatus = new TextView(this);
+                TextView paymentDate = new TextView(this);
+
+                NumberFormat df = new DecimalFormat("#.00");
+
+                paymentID.setText(p.getPaymentID() + "");
+                paymentID.setPadding(2, 0, 5, 0);
+                paymentID.setTextColor(Color.BLACK);
+                tr.addView(paymentID);
+
+                companyNum.setText(p.getContractorCONum() + "");
+                companyNum.setPadding(2, 0, 5, 0);
+                companyNum.setTextColor(Color.BLACK);
+                tr.addView(companyNum);
+
+                paymentAMT.setText(df.format(p.getPaymentAmount()) + " ");
+                paymentAMT.setPadding(2, 0, 5, 0);
+                paymentAMT.setTextColor(Color.BLACK);
+                tr.addView(paymentAMT);
+
+                proposalID.setText(p.getProposalID() + "");
+                proposalID.setPadding(2, 0, 5, 0);
+                proposalID.setTextColor(Color.BLACK);
+                tr.addView(proposalID);
+
+                paymentStatus.setText(p.getPaymentStatus() + "");
+                paymentStatus.setPadding(2, 0, 5, 0);
+                paymentStatus.setTextColor(Color.BLACK);
+                tr.addView(paymentStatus);
+
+                paymentDate.setText(p.getPaymentDate().substring(0, 10));
+                paymentDate.setPadding(2, 0, 5, 0);
+                paymentDate.setTextColor(Color.BLACK);
+                tr.addView(paymentDate);
+
+                tl.addView(tr, new TableLayout.LayoutParams(
+                        TableRow.LayoutParams.FILL_PARENT,
+                        TableRow.LayoutParams.WRAP_CONTENT));
+
+                count++;
+            }
+        }
+    }
+
+    public ArrayList<Payment> getPaymentList(){
+        DBSQLiteManager db = new DBSQLiteManager(this);
+        return db.getPaymentList();
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
--- app/src/main/java/com/ioreno/grecoantoine/ioreno/AdminTotals.java
+++ app/src/main/java/com/ioreno/grecoantoine/ioreno/AdminTotals.java
@@ -1,6 +1,8 @@
 package com.ioreno.grecoantoine.ioreno;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
@@ -8,7 +10,15 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.ioreno.grecoantoine.ioreno.DBManager.DBSQLiteManager;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 
 public class AdminTotals extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
     //  private DrawerLayout dl;
@@ -30,6 +40,70 @@ public class AdminTotals extends AppCompatActivity implements NavigationView.OnN
 
         NavigationView nv = findViewById(R.id.nav_view);
         nv.setNavigationItemSelectedListener(this);
+
+        //table
+        TableLayout tl = (TableLayout)findViewById(R.id.TotalTable);
+
+        //header
+        TableRow tr_head = new TableRow(this);
+        tr_head.setBackgroundColor(getResources().getColor(R.color.TableBlue));
+        tr_head.setLayoutParams(new TableLayout.LayoutParams(
+                TableLayout.LayoutParams.FILL_PARENT,
+                TableLayout.LayoutParams.WRAP_CONTENT));
+
+        //Header***************************************************
+        TextView number_payments = new TextView(this);
+        number_payments.setText("Number of Payments");
+        number_payments.setTextColor(Color.BLACK);
+        number_payments.setPadding(5, 5, 400, 5);
+        number_payments.setTypeface(null, Typeface.BOLD);
+        tr_head.addView(number_payments);// add the column to the table row here
+
+        TextView total_payments = new TextView(this);
+        total_payments.setText("Total payments");
+        total_payments.setTextColor(Color.BLACK);
+        total_payments.setPadding(5, 5, 30, 5);
+        total_payments.setTypeface(null, Typeface.BOLD);
+        tr_head.addView(total_payments);// add the column to the table row here
+
+        tl.addView(tr_head, new TableLayout.LayoutParams(
+                TableLayout.LayoutParams.FILL_PARENT,
+                TableLayout.LayoutParams.WRAP_CONTENT));
+
+
+        TableRow tr = new TableRow(this);
+        tr.setLayoutParams(new TableLayout.LayoutParams(
+                TableRow.LayoutParams.FILL_PARENT,
+                TableRow.LayoutParams.WRAP_CONTENT));
+        TextView labelNumPay = new TextView(this);
+        TextView labelTotPay = new TextView(this);
+
+        NumberFormat dfTotal = new DecimalFormat("#.00");
+        NumberFormat dfNumber = new DecimalFormat("#.##");
+        double total = getTotal();
+        double number = getNumber();
+        labelNumPay.setText(dfNumber.format(getNumber())+"");
+        labelNumPay.setPadding(2, 0, 5, 0);
+        labelNumPay.setTextColor(Color.BLACK);
+        tr.addView(labelNumPay);
+
+        labelTotPay.setText(dfTotal.format(total)+" $");
+        labelTotPay.setPadding(2, 0, 5, 0);
+        labelTotPay.setTextColor(Color.BLACK);
+        tr.addView(labelTotPay);
+
+        tl.addView(tr, new TableLayout.LayoutParams(
+                TableRow.LayoutParams.FILL_PARENT,
+                TableRow.LayoutParams.WRAP_CONTENT));
+    }
+
+    public double getNumber(){
+        DBSQLiteManager db = new DBSQLiteManager(this);
+        return  db.getPaymentCount();
+    }
+    public double getTotal(){
+        DBSQLiteManager db = new DBSQLiteManager(this);
+        return  db.getTotalPayments();
     }
 
     @Override
